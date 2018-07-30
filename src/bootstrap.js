21c05d88a9eb048e34b8f871cfa7aee48530e143
AyeEmNutTeeBee <teebee10@hotmail.com>
2018-07-30 11:35:55 -0600

    Updated

--- src/bootstrap.js
+++ src/bootstrap.js
@@ -2,21 +2,30 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import { Provider } from 'react-redux';
 import { createStore, applyMiddleware } from 'redux';
-import { BrowserRouter } from 'react-router-dom';
-import App from './components/app';
+import { BrowserRouter, Route, Switch } from 'react-router-dom';
 import reducers from './reducers';
 
 const createStoreWithMiddleware = applyMiddleware()(createStore);
 
-import 'bootstrap/dist/css/bootstrap.css';
+//import 'bootstrap/dist/css/bootstrap.css';
 import './style/main.scss';
+
+import Layout from './components/layout';
+
+import SignIn from './components/auth/signIn';
+import SignUp from './components/auth/signUp';
 
 
 function main() {
   ReactDOM.render(
     <Provider store={createStoreWithMiddleware(reducers)}>
       <BrowserRouter>
-        <App />
+        <Switch>
+          <Layout>
+            <Route path='/' exact component={SignIn}/>
+            <Route path='/SignUp' component={SignUp}/>
+          </Layout>
+        </Switch>
       </BrowserRouter>
     </Provider>
     , document.querySelector('.app-wrapper'));
---
+++ src/components/auth/signIn.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+import { FormTitle } from '../formTitle';
+
+class SignIn extends Component {
+    render() {
+        return (
+            <div className='sign-in'>
+                <FormTitle className="sign-in__title" text="Login"/>
+            </div>
+        )
+    }
+}
+
+export default SignIn;
