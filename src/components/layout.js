812af97dc2260c9b0f7f62994722cc1ca222a2b7
AyeEmNutTeeBee <teebee10@hotmail.com>
2018-07-31 11:53:47 -0600

    Fixed

--- src/components/layout.js
+++ src/components/layout.js
@@ -1,14 +1,31 @@
 import React, { Component } from 'react';
- import { Header, HeaderBar } from './header';
- class Layout extends Component {
+import { connect } from 'react-redux';
+
+import { Header, HeaderBar } from './header';
+
+class Layout extends Component {
     render() {
+        const { title, subtitle, hideBar } = this.props;
         return (
             <div className='layout-grid'>
-                <Header/>
-                <HeaderBar/>
+                <Header
+                    title={title}
+                    subtitle={subtitle}
+                />
+                {this.props.hideBar ? '' : <HeaderBar/>}
                 {this.props.children}
             </div>
         )
     }
 }
- export default Layout;
+ 
+function mapStateToProps(state) {
+    const header = state.header;
+    return {
+        ...header
+    }
+}
+
+Layout = connect(mapStateToProps)(Layout);
+
+export default Layout;
